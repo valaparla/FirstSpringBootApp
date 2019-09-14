65627b1cb0a9da91f2a6d2146443cb363bb1a397
elivalaparla <elivalaparla@gmail.com>
2019-09-14 01:40:08 -0500

    initial check in

--- src/main/java/Bootstrap/Application.java
+++ src/main/java/Bootstrap/Application.java
@@ -2,15 +2,21 @@ package Bootstrap;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@RestController
 //@EnableAutoConfiguration
-public class Application {
-//    @RequestMapping("/")
+public class Application extends SpringBootServletInitializer {
+    //    @RequestMapping("/")
 //    String home() {
 //        return "Hello World!";
 //    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
